a01738367a0417fba30f8d88467a9ab3efc46c69
Lary-ssa <laryreism1@gmail.com>
2022-05-05 06:35:35 -0300

    Tests commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username)} 
   end
+  describe 'sincronization' do
+    let(:username) { 'rails' }
+    before do 
+      create(:user, username: username)
+    end 
+
+    it 'Create last messages with Twitter api response' do
+      VCR.use_cassette('twitter_api') do
+        expect(User.sync_all.count).to eql(20) 
+      end
+    end
+  end 
 end
