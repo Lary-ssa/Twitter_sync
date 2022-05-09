a5f350f18b62163131511bcbcd970bf5215532ce
Lary-ssa <laryreism1@gmail.com>
2022-05-09 17:43:39 -0300

    - Removendo testes desnecessários - Corrigindo retorno do Rspec de TwitterMessages

--- spec/requests/twitter_messages_spec.rb
+++ spec/requests/twitter_messages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "TwitterMessages", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/twitter_messages/index"
+      get "/twitter_messages"
       expect(response).to have_http_status(:success)
     end
   end
